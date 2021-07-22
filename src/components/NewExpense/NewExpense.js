3aa4d85f81b24d618dd068e969a9bb7c37dd96da
tuicaodan <diaoyiwei@gmail.com>
2021-07-22 23:06:14 +1200

    added fold/unfold section to practice conditional outoput

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,21 +1,39 @@
-import React from 'react';
-import ExpenseFrom from './ExpenseForm';
-import './NewExpense.css';
+import React, { useState } from "react";
+import ExpenseFrom from "./ExpenseForm";
+import "./NewExpense.css";
 
 const NewExpense = (props) => {
-    const onSaveExpenseDateHandler = (enteredExpenseDate) => {
-        const expenseData = {
-            ...enteredExpenseDate,
-            id: Math.random().toString()
-        }
-        props.onAddExpense(expenseData);
+  const onSaveExpenseDateHandler = (enteredExpenseDate) => {
+    const expenseData = {
+      ...enteredExpenseDate,
+      id: Math.random().toString(),
     };
+    props.onAddExpense(expenseData);
+  };
 
-    return ( 
-        <div className='new-expense'>
-            <ExpenseFrom onSaveExpenseDate={onSaveExpenseDateHandler} />            
-        </div>
-     );
+  const [formVisibility, setFormVisibility] = useState(true);
+
+
+  //get form status from the children components
+  const foldFormHandler = (status) => {
+    setFormVisibility(status);
+  }; 
+  
+  const expendFormHandler = () => {
+    setFormVisibility(true);
 }
- 
+
+  return (
+    <div className="new-expense">
+      {formVisibility && (
+        <ExpenseFrom
+          onFoldForm={foldFormHandler}
+          onSaveExpenseDate={onSaveExpenseDateHandler}
+        />
+      )}
+      {!formVisibility && <button onClick={expendFormHandler}>Add New Expense</button> }
+    </div>
+  );
+};
+
 export default NewExpense;
